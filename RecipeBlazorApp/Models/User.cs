ea244c4263a3c148b1f149fe09797ac82fd45d3e
Mohamed Gallab <mohamed.w.gallab@gmail.com>
2022-09-26 23:29:04 +0300

    added register functionality

---
+++ RecipeBlazorApp/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RecipeBlazorApp.Models;
+
+public class User
+{
+	[Required]
+	public string Name { get; set; } = string.Empty;
+	[Required]
+	public string Password { get; set; } = string.Empty;
+}
