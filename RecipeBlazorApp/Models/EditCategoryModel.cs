7902df221c5a493f5b5a45a242fc9e19adf37585
Mohamed Gallab <mohamed.w.gallab@gmail.com>
2022-09-27 23:24:48 +0300

    finished category functionality

---
+++ RecipeBlazorApp/Models/EditCategoryModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RecipeBlazorApp.Models;
+
+public class EditCategoryModel
+{
+	[Required]
+	public string OldCategory { get; set; } = string.Empty;
+	[Required]
+	public string NewCategory { get; set; } = string.Empty;
+}
